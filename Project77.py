5db16226cbb8049e87112311a95af3731e6f0a7f
jbrowne09 <jbro682@aucklanduni.ac.nz>
2020-07-13 21:29:31 +1200

    Update Project77.py

--- Project77.py
+++ Project77.py
@@ -7,6 +7,11 @@ text = ""
 for page in file.pages:
     text += page.extract_text()
 
+
+###NLTK
 #tokenizer = nltk.tokenize.TreebankWordTokenizer()
 #print(tokenizer.tokenize(text))
+
 print(text)
+tokenize = nltk.tokenize.sent_tokenize(text)
+print(tokenize)
