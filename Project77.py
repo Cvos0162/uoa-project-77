be538ec9a037c1bfc4ab9137a8c1c0a2177c656c
jbrowne09 <jbro682@aucklanduni.ac.nz>
2020-07-13 20:15:02 +1200

    pdf -> text

--- Project77.py
+++ Project77.py
@@ -1,5 +1,12 @@
 import nltk
-text = "Sentence Test 1! Sentence Test 2. Sentece Test 3? Sentence Test 4"
+import pdfplumber
 
-tokenize = nltk.tokenize.sent_tokenize(text)
-print(tokenize)
+file = pdfplumber.open('test.pdf')
+text = ""
+
+for page in file.pages:
+    text += page.extract_text()
+
+#tokenizer = nltk.tokenize.TreebankWordTokenizer()
+#print(tokenizer.tokenize(text))
+print(text)
