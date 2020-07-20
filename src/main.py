7ca1a8bee82c3659817fb29fcb0e60f2d28232af
In Ha Ryu <iryu815@aucklanduni.ac.nz>
2020-07-21 09:23:50 +1200

    clean up

---
+++ src/main.py
@@ -0,0 +1,7 @@
+### main.py
+"""
+Main execution script for extraction.
+Lets make this into some kind of command line IO tool for now.
+input param: input file dir, output dir, (maybe a page number?)
+output: a xml file
+"""
---
+++ src/pdfminer_test.py
@@ -0,0 +1,19 @@
+from io import StringIO
+
+from pdfminer.layout import LAParams
+from pdfminer.pdfdocument import PDFDocument
+from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
+from pdfminer.pdfpage import PDFPage
+from pdfminer.pdfparser import PDFParser
+from pdfminer.converter import XMLConverter
+
+outfp = open('../output/output.xml', "wb")
+with open('../sample/Building Regulations 1992 p1.pdf', 'rb') as in_file:
+    parser = PDFParser(in_file)
+    doc = PDFDocument(parser)
+    rsrcmgr = PDFResourceManager()
+    device = XMLConverter(rsrcmgr, outfp, laparams=LAParams())
+    interpreter = PDFPageInterpreter(rsrcmgr, device)
+    for page in PDFPage.create_pages(doc):
+        interpreter.process_page(page)
+    device.close()
