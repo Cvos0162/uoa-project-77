432ae38de5c840550851c5888d6d8016e2a8d888
In Ha Ryu <iryu815@aucklanduni.ac.nz>
2020-10-09 06:15:02 +1300

    keras

--- src/pdfminer_test.py
+++ src/pdfminer_test.py
@@ -5,30 +5,37 @@ from pdfminer.pdfdocument import PDFDocument
 from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
 from pdfminer.pdfpage import PDFPage
 from pdfminer.pdfparser import PDFParser
-from converter import XMLConverter
+from LegalDocMLconverter import LegalDocMLconverter
 from pdfminer.converter import XMLConverter as PDFMinerConverter
 
-outfp = open('../output/sample.xml', "wb")
-with open('../sample/Building Regulations 1992 p6.pdf', 'rb') as in_file:
+input_path = "../sample/e2-external-moisture-3rd-edition-amendment-8.pdf"
+output_path = "../output/output.xml"
+output_path2 = "../output/sample.xml"
+
+img_output_path = "../output/pdfminer_page1.jpg"
+
+outfp = open(output_path, "wb")
+with open(input_path, 'rb') as in_file:
     parser = PDFParser(in_file)
     doc = PDFDocument(parser)
     rsrcmgr = PDFResourceManager()
-    device = PDFMinerConverter(rsrcmgr, outfp, laparams=LAParams())
+    device = LegalDocMLconverter(rsrcmgr, outfp, laparams=LAParams())
     interpreter = PDFPageInterpreter(rsrcmgr, device)
     for page in PDFPage.create_pages(doc):
         interpreter.process_page(page)
+    #device.draw_layout(input_path, img_output_path)
     device.close()
 outfp.close()
 
-outfp = open('../output/output.xml', "wb")
-with open('../sample/Building Regulations 1992 p6.pdf', 'rb') as in_file:
-    parser = PDFParser(in_file)
-    doc = PDFDocument(parser)
-    rsrcmgr = PDFResourceManager()
-    device = XMLConverter(rsrcmgr, outfp, laparams=LAParams())
-    interpreter = PDFPageInterpreter(rsrcmgr, device)
-    for page in PDFPage.create_pages(doc):
-        interpreter.process_page(page)
-    device.close()
-outfp.close()
+#outfp = open(output_path2, "wb")
+#with open(input_path, 'rb') as in_file:
+#    parser = PDFParser(in_file)
+#    doc = PDFDocument(parser)
+#    rsrcmgr = PDFResourceManager()
+#    device = PDFMinerConverter(rsrcmgr, outfp, laparams=LAParams())
+#    interpreter = PDFPageInterpreter(rsrcmgr, device)
+#    for page in PDFPage.create_pages(doc):
+#        interpreter.process_page(page)
+#    device.close()
+#outfp.close()
 
